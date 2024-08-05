efab0494a78860fc5f21e0c54d6b36716235e488
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-04 23:36:42 -0700

    an implementation of radixSort

--- radix.js
+++ radix.js
@@ -1,3 +1,36 @@
-function radixSort() {}
+/** helper function: bucket everything
+ * takes an array and a power of two to bucket it by
+ */
+
+function radixBucket(arr, power) {
+    // create our buckets
+    const buckets = [ [], [] ]
+    
+    for (val of arr) {
+        if (Math.floor((val % 2**(power+1))/2**power) > 0) {
+            buckets[1].push(val);
+        }
+        else buckets[0].push(val);
+    }
+    
+    return {
+        arr: [...buckets[0], ...buckets[1] ],
+        hasLarge: (buckets[1].length > 0) ? true : false
+    }
+}
+
+function radixSort(arr) {
+    let needToRepeat = true;
+    let i = 0;
+    let workingArray = [...arr]
+    
+    while (needToRepeat) {
+        const buckets = radixBucket(workingArray, i)
+        workingArray = buckets.arr;
+        needToRepeat = buckets.hasLarge;
+        i++;
+    }
+    return workingArray;
+}
 
 module.exports = radixSort;

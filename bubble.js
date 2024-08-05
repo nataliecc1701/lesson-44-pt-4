2dc05fba2802bbc137649ace7fbbab1bd47df026
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-04 21:40:58 -0700

    bubblesort works

--- bubble.js
+++ bubble.js
@@ -1,3 +1,17 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    let swapped = false;
+    do {
+        swapped = false;
+        let i = 0;
+        while(i+1 < arr.length) {
+            if(arr[i] > arr[i+1]) {
+                swapped = true;
+                [ arr[i], arr[i+1] ] = [ arr[i+1], arr[i] ]
+            }
+            i++;
+        }
+    } while (swapped)
+    return arr;
+}
 
 module.exports = bubbleSort;

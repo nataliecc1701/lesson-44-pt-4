ecaac4479616bb2fa45e887f8b91eb1a8c37aadf
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-04 22:28:00 -0700

    implemented selectionSort

--- selection.js
+++ selection.js
@@ -1,3 +1,20 @@
-function selectionSort() {}
+function selectionSort(arr) {
+    for (i = 0; i < arr.length; i++) {
+        // keep track of our lowest-seen-value and its index;
+        let min = arr[i];
+        let mindex = i;
+        
+        for (j = i; j < arr.length; j++) {
+            if (arr[j] < min) {
+                min = arr[j];
+                mindex = j
+            }
+        }
+        if (mindex !== i) {
+            [ arr[i], arr[mindex] ] = [ arr[mindex], arr[i] ]
+        }
+    }
+    return arr;
+}
 
 module.exports = selectionSort;

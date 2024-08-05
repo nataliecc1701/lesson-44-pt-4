48a03d58323f5623ac80f2e66d70300bbc5df5f2
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-04 23:59:19 -0700

    radixSort implemented and passes all tests

--- radix.js
+++ radix.js
@@ -3,25 +3,49 @@
  */
 
 function getDigit(num, place) {
-    
+    return Math.floor((num%10**(place+1))/(10**place))
 }
 
 /** takes an integer num and returns its number of digits */
 
 function digitCount(num) {
-    return Math.ceil(Math.log10(num));
+    return Math.ceil(Math.log10(num+1));
 }
 
 /** takes an array of integers and returns the entry with the most digits */
 
-function mostDigits() {
-    
+function mostDigits(arr) {
+    let mostSeen = 0;
+    for (val of arr) {
+        const curr = digitCount(val)
+        if (curr > mostSeen) mostSeen=curr;
+    }
+    return mostSeen;
+}
+
+/** divide an array into buckets based on place */
+
+function divideArrayIntoBuckets(arr, place) {
+    // make our buckets
+    const buckets = []
+    while (buckets.length < 10) {
+        buckets.push([])
+    }
+    for (val of arr) {
+        buckets[getDigit(val, place)].push(val)
+    }
+    return buckets;
 }
 
 /** non-comparative sort for integers */
 
-function radixSort() {
+function radixSort(arr) {
+    const most = mostDigits(arr);
     
+    for(i = 0; i <= most; i++) {
+        arr = divideArrayIntoBuckets(arr, i).flat()
+    }
+    return arr;
 }
 
 module.exports = { getDigit, digitCount, mostDigits, radixSort }
